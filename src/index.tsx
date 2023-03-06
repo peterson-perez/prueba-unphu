a12f36bd4628e234b02d7bfb84990eec2cddd6bc
peterson-perez <p.perez60@unapec.edu.do>
2023-03-06 19:26:17 +0100

    feat: routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AddUser from './pages/AddUser';
+import Dashboard from './pages/Dashboard';
+import ListUsers from './pages/ListUsers';
+import Login from './pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+    errorElement: <h1>Esta pagina no existe!!!</h1>
+  },
+  {
+    path: '/Dashboard',
+    element: <Dashboard />
+  },
+  {
+    path: '/ListUser',
+    element: <ListUsers />
+  },
+  {
+    path: '/AddUser',
+    element: <AddUser />
+  }
+])
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <RouterProvider router={router} />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
