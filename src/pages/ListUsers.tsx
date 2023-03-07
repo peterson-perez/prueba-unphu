c620ad732113d389898c4de81a78569ebfde67f1
peterson-perez <p.perez60@unapec.edu.do>
2023-03-07 13:02:44 +0100

    feat: UserList, header and menu sidebar

---
+++ src/pages/ListUsers.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import './ListUsers.css';
+
+const ListUsers = () => {
+
+    return (
+        <>
+            <header>
+                <Header />
+            </header>
+
+            <body>
+                <nav className="navbar bg-body-tertiary">
+                    <div className="container-fluid justify-content-start">
+                        <Link className="btn btn-outline-success me-2" to={'/AddUser'}>Agregar usuario</Link>
+                    </div>
+                </nav>
+
+                <h1>Lista de usuarios</h1>
+                <hr />
+
+                <table>
+                    <thead>
+                        <th>
+                            Nombre
+                        </th>
+                    </thead>
+                    <tbody>
+                        <td>
+                            Carlos
+                        </td>
+                    </tbody>
+                </table>
+            </body>
+        </>
+    )
+}
+export default ListUsers;
