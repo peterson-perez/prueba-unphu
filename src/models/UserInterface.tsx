df1c6e01c3352e4f41c53b1cf2f60baa5ab26cef
peterson-perez <p.perez60@unapec.edu.do>
2023-03-08 14:11:56 +0100

    feat: UserInterface

---
+++ src/models/UserInterface.tsx
@@ -0,0 +1,14 @@
+export interface UserInterface {
+    name: string;
+    firstLastName: string;
+    secondLastName: string;
+    id: number;
+    age: number;
+    gender: string;
+    address: string;
+    telephone: number;
+    email: string;
+    civilStatus: string;
+    children: string;
+    birthday: string;
+}
