2bb0a48a7e73885d8b52f47757e4977b08e2c205
peterson-perez <p.perez60@unapec.edu.do>
2023-03-12 11:06:36 +0100

    feat: user table list

--- src/pages/ListUsers.tsx
+++ src/pages/ListUsers.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import './css/ListUsers.css';
 
-const ListUsers = () => {
+const ListUsersTable = () => {
 
     return (
         <>
@@ -10,30 +10,42 @@ const ListUsers = () => {
                 <Header />
             </header>
 
+
             <body>
-                <nav className="navbar bg-body-tertiary">
-                    <div className="container-fluid justify-content-start">
-                        <Link className="btn btn-outline-success me-2" to={'/AddUser'}>Agregar usuario</Link>
-                    </div>
-                </nav>
 
-                <h1>Lista de usuarios</h1>
-                <hr />
-
-                <table>
+                <table className="table table-light">
                     <thead>
-                        <th>
-                            Nombre
-                        </th>
+                        <tr >
+                            <th scope="col">Id</th>
+                            <th scope="col">Nombre completo</th>
+                            <th scope="col">Correo electronico</th>
+                            <th scope="col">Genero</th>
+                        </tr>
                     </thead>
                     <tbody>
-                        <td>
-                            Carlos
-                        </td>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Piterson perez Ramirez</td>
+                            <td>dwasd@hotmail.com</td>
+                            <td>Hombre</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Wanda Sanchez</td>
+                            <td>Wanda@hotmail.com</td>
+                            <td>Mujer</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry the Bird</td>
+                            <td>@twitter</td>
+                            <td>Hombre</td>
+                        </tr>
                     </tbody>
                 </table>
+
             </body>
         </>
     )
 }
-export default ListUsers;
+export default ListUsersTable;
