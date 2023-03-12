32197643b3e2f39ca46162f913c0a45621b6406d
peterson-perez <p.perez60@unapec.edu.do>
2023-03-12 20:21:15 +0100

    fix: user validations

--- src/components/validations/UserValidation.tsx
+++ src/components/validations/UserValidation.tsx
@@ -4,31 +4,31 @@ const UserValidation = (values: any) => {
 
         if (!values.name) {
             errors.name = 'Este campo es obligatorio';
-        }else if(!/^[A-Z]{2,14}$/i.test(values.name)){
-            errors.name = 'El nombre solo puede tener letras y de 2 a 14 caracteres';
+        } if(!/^[A-Z]{2,20}$/i.test(values.name)){
+            errors.name = 'El nombre solo puede tener letras y de 2 a 20 caracteres';
         }
 
         if (!values.firstLastName) {
             errors.firstLastName = 'Este campo es obligatorio';
-        }else if(!/^[A-Z]{2,14}$/i.test(values.firstLastName)){
-            errors.firstLastName = 'El nombre solo puede tener letras y de 2 a 14 caracteres';
+        } if(!/^[A-Z ]{2,20}$/i.test(values.firstLastName)){
+            errors.firstLastName = 'El nombre solo puede tener letras y de 2 a 20 caracteres';
         }  
 
         if (!values.secondLastName) {
             errors.secondLastName = 'Este campo es obligatorio';
-        }else if(!/^[A-Z]{2,14}$/i.test(values.secondLastName)){
-            errors.secondLastName = 'El nombre solo puede tener letras y de 2 a 14 caracteres';
+        } if(!/^[A-Z ]{2,20}$/i.test(values.secondLastName)){
+            errors.secondLastName = 'El nombre solo puede tener letras y de 2 a 20 caracteres';
         }  
 
         if (!values.id) {
             errors.id = 'Este campo es obligatorio';
-        }else if(!/^[0-9]{11,11}$/i.test(values.id)){
-            errors.id = 'la cedula solo puede tener numeros y 11 caracteres';
+        } if(!/^[0-9]{11,11}$/i.test(values.id)){
+            errors.id = 'la cedula solo puede tener numeros y solo 11 caracteres';
         } 
 
         if (!values.age) {
             errors.age = 'Este campo es obligatorio';
-        }else if(!/^[0-9]{1,2}$/i.test(values.age)){
+        } if(!/^[0-9]{1,2}$/i.test(values.age)){
             errors.age = 'la cedula solo puede tener numeros y entre 1 y 2 caracteres';
         } 
 
@@ -38,8 +38,8 @@ const UserValidation = (values: any) => {
 
         if (!values.address) {
             errors.address = 'Este campo es obligatorio';
-        }else if(!/^[A-Z0-9]{1,2}$/i.test(values.address)){
-            errors.address = 'la cedula solo puede tener numeros y entre 1 y 2 caracteres';
+        } if(!/^[A-Z0-9/# ]{10,50}$/i.test(values.address)){
+            errors.address = 'la cedula solo puede tener numeros y entre 10 y 50 caracteres';
         }
 
         if (!values.birthday) {
@@ -48,14 +48,14 @@ const UserValidation = (values: any) => {
 
         if (!values.email) {
             errors.email = 'Este campo es obligatorio';
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        } if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
             errors.email = 'Solo puede contener numeros, letras, guiones, guiones bajos y puntos';
         };
 
         if (!values.telephone) {
             errors.telephone = 'Este campo es obligatorio';
-        }else if(!/^[0-9]{10,13}$/i.test(values.telephone)){
-            errors.telephone = 'El telefono debe contener entre 10 y 13 caracteres';
+        } if(!/^[0-9]{10,10}$/i.test(values.telephone)){
+            errors.telephone = 'El telefono debe contener 10 caracteres';
         }
 
         if (!values.civilStatus) {
@@ -64,6 +64,8 @@ const UserValidation = (values: any) => {
 
         if(!values.children){
            errors.children = 'Este campo es obligatorio';
+        } if(!/^[0-9]{1,2}$/i.test(values.children)){
+           errors.children = 'Este campo solo puede tener numeros y entre 1 y 2 caracteres'
         }
         
         return errors
