0d680eeb165d32252e17477761afa89f04780684
peterson-perez <p.perez60@unapec.edu.do>
2023-03-13 11:37:22 +0100

    rename camelCase to snake_case

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './app';
 import { store } from './redux/store';
 
 
